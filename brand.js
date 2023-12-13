cae47ce69a6a1bc8a80adb21270d6c57e46d17db
rgpv20 <140268665+rgpv20@users.noreply.github.com>
2023-12-13 20:44:56 +0530

    Add files via upload

---
+++ brand.js
@@ -0,0 +1,19 @@
+const img=document.querySelector("img");
+const cursor=document.querySelector("#cursor");
+
+img.addEventListener("mousemove",function(dets){
+    const rotx  = 58 - dets.x/11;
+    const roty =Math.abs(rotx)
+     img.style.transform=`rotateX(${rotx}deg) rotateY(${-roty}deg)`
+
+
+})
+img.addEventListener("mouseleave",function(dets){
+    setTimeout(function(){
+    img.style.transform=`rotateX(0deg) rotateY(0deg)`
+},2000)
+})
+document.addEventListener("mousemove",function(dets){
+  cursor.style.left=`${dets.x}px`
+  cursor.style.top=`${dets.y}px`
+})
